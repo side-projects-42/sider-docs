c14d33978799d568bb602fb8c41954c3a21229b2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-07-26 16:07:31 +0900

    Fix company name on footer (#644)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -78,7 +78,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Sider, Inc.`,
+  copyright: `Copyright © ${new Date().getFullYear()} Sider Corporation`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
